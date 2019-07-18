f0c609191720e389c039b1225b310bfd117901a1
renilsonalbuquerque <renilson.albuquerque@pitang.com>
2019-07-18 19:10:06 -0300

    Cria a estrutura base versão Web

--- shakal-rpg-web/src/app/app-routing.module.ts
+++ shakal-rpg-web/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BaseComponent } from './modules/layout/base/base.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: BaseComponent,
+    children:[
+      {
+        path: 'monster',
+        loadChildren: './modules/monster/monster.module#MonsterModule'
+      }
+    ]
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
