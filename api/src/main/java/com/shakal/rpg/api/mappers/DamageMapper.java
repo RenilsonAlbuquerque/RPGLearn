239874a7a7f44ea6da9e10dde5c83389f07aec3d
renilsonalbuquerque <renilson.albuquerque@pitang.com>
2019-07-22 19:25:41 -0300

    Tradução dos tipos de dano

--- api/src/main/java/com/shakal/rpg/api/mappers/DamageMapper.java
+++ api/src/main/java/com/shakal/rpg/api/mappers/DamageMapper.java
@@ -2,6 +2,7 @@ package com.shakal.rpg.api.mappers;
 
 import com.shakal.rpg.api.dto.commons.KeyValueDTO;
 import com.shakal.rpg.api.model.DamageType;
+import com.shakal.rpg.api.model.enums.DamageTypeEnum;
 import com.shakal.rpg.api.model.relation.AttackDice;
 
 public class DamageMapper {
@@ -14,7 +15,56 @@ public class DamageMapper {
 	public static KeyValueDTO  entityTODTO(DamageType entity) {
 		KeyValueDTO result = new KeyValueDTO();
 		result.setId(entity.getId());
-		result.setValue(entity.getValue().toString());
+		result.setValue(translateDamage(entity.getValue()));
 		return result;
+	}
+	
+	public static String translateDamage(DamageTypeEnum enumInput) {
+		String result = "";
+		switch(enumInput) {
+			case ACID: 
+				result = "Ácido";
+				break;
+			case BLUDGEONING: 
+				result = "Concussão";
+				break;
+			case COLD: 
+				result = "Gelo";
+				break;
+			case FIRE: 
+				result = "Fogo";
+				break;
+			case FORCE: 
+				result = "Força";
+				break;
+			case LIGHTNING: 
+				result = "Elétrico";
+				break;
+			case NECROTIC:
+				result = "Necrótico";
+				break;
+			case PIERCING: 
+				result = "Perfurante";
+				break;
+			case POISON: 
+				result = "Veneno";
+				break;
+			case PSYCHIC:
+				result = "Psíquico";
+				break;
+			case RADIANT: 
+				result = "Radiante";
+				break;
+			case SLASHING: 
+				result = "Veneno";
+				break;
+			case THUNDER:
+				result = "Trovão";
+				break;
+			default:
+				break;
+		}
+		return result;
+		
 	}
 }
